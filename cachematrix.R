3b5b1c8419e3d81a8b64e78ef691711af91ebc79
gavin099 <54856768+gavin099@users.noreply.github.com>
2021-07-15 01:27:21 -0400

    Solve the problem!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The function would first record the inverse of the input matrix,
+# store it in cache, then output its value in cacheSolve function.
 
-## Write a short comment describing this function
-
+# This is the first step of the whole function: calculates the inverse 
+# and stores its value
 makeCacheMatrix <- function(x = matrix()) {
-
+  if(ncol(x) == nrow(x) && det(x)!= 0){
+  m <- NULL
+  setmatrix <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  getmatrix <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(setmatrix = setmatrix, getmatrix = getmatrix,
+       setinverse = setinverse,
+       getinverse = getinverse)}
+  else{
+    message("The matrix you've put is singular.")
+  }
 }
 
 
 ## Write a short comment describing this function
-
+# Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$getmatrix()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m    
 }
